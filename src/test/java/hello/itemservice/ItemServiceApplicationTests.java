9462972342c364d419c50614ea7b46b85dee07d8
신희성 <vvwv3092@gmail.com>
2025-01-10 16:40:13 +0900

    add: test code database env

--- src/test/java/hello/itemservice/ItemServiceApplicationTests.java
+++ src/test/java/hello/itemservice/ItemServiceApplicationTests.java
@@ -3,7 +3,7 @@ package hello.itemservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest // -> Main에 있는 Spring Application을 찾아서 실행시킨다.
 class ItemServiceApplicationTests {
 
 	@Test
