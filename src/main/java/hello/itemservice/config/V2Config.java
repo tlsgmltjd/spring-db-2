bc5c5cd1eeaeee0ade456121ca324c06b45cc43f
신희성 <vvwv3092@gmail.com>
2025-01-14 14:21:07 +0900

    add: spring data jpa + querydsl v2 example

---
+++ src/main/java/hello/itemservice/config/V2Config.java
@@ -0,0 +1,39 @@
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV1;
+import hello.itemservice.repository.v2.ItemQueryRepositoryV2;
+import hello.itemservice.repository.v2.ItemRepositoryV2;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV2;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class V2Config {
+
+    private final EntityManager em;
+    private final ItemRepositoryV2 itemRepositoryV2; // spring data jpa가 자동 빈 등록해줌
+    private final DataSource dataSource;
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV2(itemRepositoryV2, itemQueryRepositoryV2());
+    }
+
+    @Bean
+    public ItemQueryRepositoryV2 itemQueryRepositoryV2() {
+        return new ItemQueryRepositoryV2(em);
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JdbcTemplateItemRepositoryV1(dataSource);
+    }
+
+}
